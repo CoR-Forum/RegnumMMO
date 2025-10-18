bd4dccb5664c69924fe6705e5aa1c4ca36c0f8da
Joshua Treudler <joshua@treudler.net>
2025-10-18 20:53:25 +0200

    security: enable secure cookies for session management

--- server.js
+++ server.js
@@ -60,7 +60,7 @@ app.use(session({
   resave: false,
   saveUninitialized: false,
   cookie: {
-    secure: false, // Set to false for HTTP; change to true if using HTTPS
+    secure: true, // Set to false for HTTP; change to true if using HTTPS
     httpOnly: true,
     sameSite: 'strict',
     maxAge: 24 * 60 * 60 * 1000 // 24 hours
